ac25116189d303389d581dc14cbb5b79e437fb51
discoverdefiteam <discover.defi@gmail.com>
2023-09-08 03:07:53 -0400

    add hard-nett config

--- site.config.js
+++ site.config.js
@@ -1,35 +1,38 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Hard-nett",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Software Protocol Engineer",
+    bio: "I enjoy building towards a soviergn future.",
+    email: "hard-nett@skiff.com",
+    github: "hard-nett",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `permissionlessweb-team`,
+      href: "https://permissionless.terp.network",
     },
+    {
+      name: `glassflow`,
+      href: "https://github.com/glassflow",
+    }
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "hard-nett's portfolio",
+    description: "welcome to hard-nett's portfolio",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://hard-nett-portfolio.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
   notionConfig: {
+    // @ts-ignore
     pageId: process.env.NOTION_PAGE_ID,
   },
 
@@ -37,12 +40,14 @@ const CONFIG = {
   googleAnalytics: {
     enable: false,
     config: {
+      // @ts-ignore
       measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
     },
   },
   googleSearchConsole: {
     enable: false,
     config: {
+      // @ts-ignore
       siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
     },
   },
@@ -61,6 +66,7 @@ const CONFIG = {
       appid: "", // Embed Code -> data-app-id value
     },
   },
+  // @ts-ignore
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
   revalidateTime: 21600 * 7, // revalidate time for [slug], index
 }
